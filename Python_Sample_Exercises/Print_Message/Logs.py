8b1f519542895eb3e21c674f73d061c67e48d37d
Soi Sheng Leong <soishengleong96@gmail.com>
2024-04-26 13:16:57 +0800

    Modify Logs.py in Python_Sample_Exercises/Print_Message folder
    Modify Logs.py to enhance readability of the script
    
    Signed-off-by: Soi Sheng Leong <soishengleong96@gmail.com>

--- Python_Sample_Exercises/Print_Message/Logs.py
+++ Python_Sample_Exercises/Print_Message/Logs.py
@@ -3,4 +3,6 @@ import inspect
 import os
 
 def log(cmd):
-    print("\n" + os.path.abspath(str(inspect.getframeinfo(inspect.currentframe().f_back).filename)) + ", line " + str(inspect.currentframe().f_back.f_lineno) + ":\n" + cmd)
+    Source_File = os.path.abspath(str(inspect.getframeinfo(inspect.currentframe().f_back).filename))
+    Source_Line_Num = str(inspect.currentframe().f_back.f_lineno)
+    print("\n" + Source_File + ", line " + Source_Line_Num + ":\n" + cmd)
